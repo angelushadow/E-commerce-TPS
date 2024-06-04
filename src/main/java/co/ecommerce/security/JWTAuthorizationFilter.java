0d06c3ce2a409c54b1b0388234f897044ad0a22d
John Ocampo <bayron.ocampo.8@gmail.com>
2024-06-04 11:47:51 -0500

    Primer commit

--- src/main/java/co/ecommerce/security/JWTAuthorizationFilter.java
+++ src/main/java/co/ecommerce/security/JWTAuthorizationFilter.java
@@ -81,52 +81,6 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
 		return true;
 	}
 	
-	
-	////////////////////////////////////////////////////
 
-//	@Autowired
-//	private JwtService jwtService;
-//	private final HandlerExceptionResolver handlerExceptionResolver;
-//	private final UserDetailsService userDetailsService;
-//
-//	public JWTAuthorizationFilter(UserDetailsService userDetailsService,
-//			HandlerExceptionResolver handlerExceptionResolver) {
-//		this.userDetailsService = userDetailsService;
-//		this.handlerExceptionResolver = handlerExceptionResolver;
-//	}
-//
-//	@Override
-//	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
-//			@NonNull FilterChain filterChain) throws ServletException, IOException {
-//		final String authHeader = request.getHeader("Authorization");
-//
-//		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-//			filterChain.doFilter(request, response);
-//			return;
-//		}
-//
-//		try {
-//			final String jwt = authHeader.substring(7);
-//			final String userEmail = jwtService.extractUsername(jwt);
-//
-//			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//
-//			if (userEmail != null && authentication == null) {
-//				UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-//
-//				if (jwtService.isTokenValid(jwt, userDetails)) {
-//					UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
-//							null, userDetails.getAuthorities());
-//
-//					authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//					SecurityContextHolder.getContext().setAuthentication(authToken);
-//				}
-//			}
-//
-//			filterChain.doFilter(request, response);
-//		} catch (Exception exception) {
-//			handlerExceptionResolver.resolveException(request, response, null, exception);
-//		}
-//	}
 
 }
